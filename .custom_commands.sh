7ee1ded136a354d83522890e486f5786b586f76b
Francisco Lagos Vilaboa <flagosvilaboa@gmail.com>
2024-03-14 10:55:30 -0700

    add push and commit commands

--- .custom_commands.sh
+++ .custom_commands.sh
@@ -9,3 +9,19 @@ function gs() {
 function ga() {
   git add $1
 }
+
+#it calls git push
+function gp() {
+  git push
+}
+
+#it calls git commit -m 
+#check if i can remove adding the " "
+function gc() {
+  git commit -m "$1"
+}
+
+function gac () {
+    git add .
+    git commit -m "$1"
+}
